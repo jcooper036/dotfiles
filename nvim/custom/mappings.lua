4c8c8690bef5de896bdecc04e8d6a97623284887
jcooper036 <jcooper036>
2026-01-02 15:15:49 -0700

    add dap, and better error handing for custom mappings

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -1,32 +1,54 @@
 local C = {
-  {
-    modes = { "n" },
-    keybind = "<leader>kk",
-    mapping_info = "<cmd> Telescope keymaps <CR>",
-    -- this is telling it to go in the Telescope category under Keymaps
-    desc = "Telescope keymaps",
-  },
-  {
-    modes = { "n" },
-    keybind = ";",
-    mapping_info = ":",
-    desc = "enter command mode",
-    opts = { nowait = true },
-  },
-  {
-    modes = { "v" },
-    keybind = ">",
-    mapping_info = ">gv",
-    desc = "indent",
-  },
+	{
+		modes = { "n" },
+		lhs = "<leader>kk",
+		rhs = "<cmd> Telescope keymaps <CR>",
+		-- this is telling it to go in the Telescope category under Keymaps
+		opts = { desc = "Telescope keymaps" },
+	},
+	{
+		modes = { "n" },
+		lhs = ";",
+		rhs = ":",
+		opts = { desc = "enter command mode", nowait = true },
+	},
+	{
+		modes = { "v" },
+		lhs = ">",
+		rhs = ">gv",
+		opts = { desc = "indent" },
+	},
+	{
+		modes = { "n" },
+		lhs = "<lseader>db",
+		rhs = function()
+			require("dap").toggle_breakpoint()
+		end,
+		opts = { desc = "Toggle breakpoint" },
+	},
+	{
+		modes = { "n" },
+		lhs = "<leader>dc",
+		rhs = function()
+			require("dap").continue()
+		end,
+		opts = { desc = "Start/contine debugging" },
+	},
 }
 
 local map = vim.keymap.set
 
 for _, mp in ipairs(C) do
-  local opts = mp.opts or {}
-  opts.desc = mp.desc
-  map(mp.modes, mp.keybind, mp.mapping_info, opts)
+	local opts = mp.opts or {}
+	local ok, error = xpcall(function()
+		map(mp.modes, mp.lhs, mp.rhs, opts)
+	end, debug.traceback)
+	if not ok then
+		vim.notify(
+			string.format("ERROR: Custom mapping failed for '%s': %s", vim.inspect(mp) or "unknwon", error),
+			vim.log.levels.ERROR
+		)
+	end
 end
 
 return C
