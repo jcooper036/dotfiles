c4369fa46f84598b87c27677ccffdf6a89f4e65c
jcooper036 <jcooper036>
2026-01-02 11:20:12 -0700

    add ruff-format and ruff-fix to config

--- nvim/custom/configs/conform.lua
+++ nvim/custom/configs/conform.lua
@@ -10,6 +10,8 @@ local options = {
 		html = { "prettier" },
 
 		sh = { "shfmt" },
+
+		python = { "ruff_fix", "ruff_format" },
 	},
 
   -- adding same formatter for multiple filetypes can look too much work for some
