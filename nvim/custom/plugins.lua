f2e073766e42219be63e4679f56609aee6de248f
jcooper036 <jcooper036>
2026-01-02 10:25:16 -0700

    add markdown preview plugin

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,77 +1,82 @@
 local plugins = {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local cmp = require("cmp")
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local cmp = require("cmp")
 
-      -- Get NvChad's default config
-      local default_opts = require("nvchad.configs.cmp")
+			-- Get NvChad's default config
+			local default_opts = require("nvchad.configs.cmp")
 
-      -- Override just the mappings
-      default_opts.mapping["<Tab>"] = cmp.mapping.confirm({ select = true })
-      default_opts.mapping["<C-n>"] = cmp.mapping.select_next_item()
-      default_opts.mapping["<C-p>"] = cmp.mapping.select_prev_item()
-      default_opts.mapping["<CR>"] = cmp.mapping({
-        i = function(fallback)
-          fallback()
-        end,
-      })
+			-- Override just the mappings
+			default_opts.mapping["<Tab>"] = cmp.mapping.confirm({ select = true })
+			default_opts.mapping["<C-n>"] = cmp.mapping.select_next_item()
+			default_opts.mapping["<C-p>"] = cmp.mapping.select_prev_item()
+			default_opts.mapping["<CR>"] = cmp.mapping({
+				i = function(fallback)
+					fallback()
+				end,
+			})
 
-      return default_opts
-    end,
-  },
+			return default_opts
+		end,
+	},
 
-  {
-    "tpope/vim-dadbod",
-    lazy = false,
-  },
+	{
+		"tpope/vim-dadbod",
+		lazy = false,
+	},
 
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
 
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "sql", "mysql", "plsql" },
-        callback = function()
-          require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
-        end,
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "sql", "mysql", "plsql" },
+				callback = function()
+					require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+				end,
+			})
+		end,
+	},
 
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = "cd app && ./install.sh",
+	},
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
-
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
